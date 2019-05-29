4f09c7ae07e8b15f79c628610bf21882f0bb86ec
shupik <edo4eddie@gmail.com>
2019-05-29 07:00:35 -0400

    Added server full status

--- VnllaBot.py
+++ VnllaBot.py
@@ -33,21 +33,27 @@ def vnllastatus():
 async def vnllastatusloop():
 	await client.wait_until_ready()
 	downtime = 0
+	uptime = 0
 	while not client.is_closed:
-		if vnllastatus() == True:
+		if vnllastatus() == False:
 			await client.change_presence(status=discord.Status.do_not_disturb, game=discord.Game(name="Down for {a} min".format(a=downtime)))
-			if downtime < 5:
+			if downtime < 10:
 				await client.change_presence(status=discord.Status.idle, game=discord.Game(name="Down for {a} min".format(a=downtime)))
-			if downtime == 5:
+			if downtime == 10:
 				for people in notifylist:
-					await client.send_message(await client.get_user_info(people), "Vnlla has been down for 5 minutes.")
+					await client.send_message(await client.get_user_info(people), "Vnlla has been down for 10 minutes.")
 			downtime += 1
 		else:
-			await client.change_presence(status=discord.Status.online, game=discord.Game(name="Server Online"))
-			if downtime > 0:
+			if downtime > 10:
 				for people in notifylist:
 					await client.send_message(await client.get_user_info(people), "Vnlla is back online.")
+					uptime = 0
+			await client.change_presence(status=discord.Status.online, game=discord.Game(name="Server Online for {a} min".format(a=uptime)))
+			if open(status,"rb").read() == open("vnllastatusfull.png","rb").read():
+				await client.change_presence(status=discord.Status.online, game=discord.Game(name="Server Full, Online for {a} min".format(a=uptime)))
 			downtime = 0
+			uptime += 1
+
 		await asyncio.sleep(60)
 
 @client.event
@@ -64,7 +70,7 @@ async def vnlla(ctx):
 @client.command(pass_context = True)
 async def add(ctx):
 	global notifylist
-	notifylist.append(ctx.message.author.id)
+	notifylist.append(int(ctx.message.author.id))
 	with open(fileName, "w") as f:
 			json.dump(notifylist, f)
 	await client.say("You've been added to the server notification list.\nUse `!remove` to get off the list.")
@@ -72,10 +78,10 @@ async def add(ctx):
 @client.command(pass_context = True)
 async def remove(ctx):
 	global notifylist
-	notifylist.remove(ctx.message.author.id)
+	notifylist.remove(int(ctx.message.author.id))
 	with open(fileName, "w") as f:
 			json.dump(notifylist, f)
 	await client.say("You've been removed from the server notification list.\nUse `!add` to get on the list.")
 
 client.loop.create_task(vnllastatusloop())
-client.run("Blocked")
+client.run("Hidden")
