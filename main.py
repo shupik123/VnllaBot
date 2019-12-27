a1b379b459e6019d6b41133f6d184935791ea8c7
Xelada <alexdalat@gmail.com>
2019-12-27 16:46:33 -0500

    c

--- main.py
+++ main.py
@@ -297,13 +297,12 @@ async def stats(ctx, stop_time=-1.0, stop_u ='d', regression=''):
 	index, t = min(enumerate(temp_pd['x']), key=lambda x:abs(x[1]-i))
 	
 	for _ in range(int(t), int(time.time())):
-		if temp_pd['x'][index] > t:
+		if (0 <= index < len(temp_pd['x'])) and (temp_pd['x'][index] > t):
 			data_x.append(t);
 			data_y.append(7);
 		else:
-			if 0 <= index < len(temp_pd['x']):
-				data_x.append((temp_pd['x'][index] - time.time()))
-				data_y.append(temp_pd['y'][index])
+			data_x.append((temp_pd['x'][index] - time.time()))
+			data_y.append(temp_pd['y'][index])
 			index += 1;
 		t += 30;
 
