1803759899f2c2af0b0b1430c3496e64531bf1e8
Xelada <alexdalat@gmail.com>
2019-12-27 17:52:41 -0500

    f

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ async def stats(ctx, stop_time=-1.0, stop_u ='d', regression=''):
 	for _ in range(int(t), int(time.time()), 30):
 		print(index)
 		print(len(temp_pd['x']))
-		if index >= len(temp_pd['x']) or temp_pd['x'][index] > t:
+		if index >= len(temp_pd['x']) or temp_pd['x'][index] < t:
 			data_x.append(t-time.time());
 			#data_y.append(temp_pd['y'][index-20160]);
 			data_y.append(40);
