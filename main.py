ece52d9099a16a575488ba652bd3bdc626b36888
Xeladarocks <xeladarocks@gmail.com>
2019-12-30 10:44:59 -0500

    test

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ async def stats(ctx, stop_time=-1.0, stop_u ='d', regression=''):
 	index, t = min(enumerate(temp_pd['x']), key=lambda x:abs(x[1]-i)) # find time in data that is closest to requested time
 	
 	for _ in range(int(t), int(time.time()), 30):
-		if index > len(temp_pd['x']) or temp_pd['x'][index] < t:
+		if index >= len(temp_pd['x']) or temp_pd['x'][index] <= t:
 			data_x.append(t-time.time())
 			data_y.append(temp_pd['y'][index- (20160 if len(temp_pd['x']) > 20160 else len(temp_pd['x']))]) # data 1 week ago
 			#data_y.append(20);
